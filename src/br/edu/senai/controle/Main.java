e1dbe861d23e4f1bbf190270e65255433f33bde8
Jean <lopesjean.jl@gmail.com>
2019-12-04 20:28:31 -0300

    Estruturação do main.SCV

--- src/br/edu/senai/controle/Main.java
+++ src/br/edu/senai/controle/Main.java
@@ -1,19 +1,76 @@
 package br.edu.senai.controle;
+//
+//import java.io.IOException;
+//
+//import br.edu.senai.modelo.ManipuladorArquivo;
+//
+//public class Main {
+// 
+//    public static void main(String args[]) throws IOException {
+//        String path = "C:/Users/Senai/Documents/Jean/ControleFinanceiro/file.txt";
+// 
+//        ManipuladorArquivo.leitor(path);
+//        ManipuladorArquivo.escritor(path);
+//        ManipuladorArquivo.leitor(path);
+//    }
+// 
+//}
 
-import java.io.IOException;
+import java.util.Scanner;
 
-import br.edu.senai.modelo.ManipuladorArquivo;
+// criar um projeto onde será passado o valor em caixa, o valor adicionado, o valor retirado e retornar o total instantâneo. 
+
 
 public class Main {
- 
-    public static void main(String args[]) throws IOException {
-        String path = "C:/Users/Senai/Documents/Jean/ControleFinanceiro/file.SCV";
- 
-        ManipuladorArquivo.leitor(path);
-        ManipuladorArquivo.escritor(path);
-        ManipuladorArquivo.leitor(path);
-    }
- 
+
+  public static void main(String[] args) {
+
+//    Main obj = new Main();
+//    obj.run();
+
+	  System.out.println("Bem vindo a planilha de gastos");
+	  
+	  // carregar o banco de dados // criar um get leitura de arquivo 
+	  
+	  // perguntar oq ele quer fazer 
+	//  System.out.println("Informe oque vc deseja fazer: \n\tS - Para retornar valor de saldo \n\tG - Para adicionar novo gasto \n\tR - para adicionar um novo rendimento \n\tQ - Para encerrar o programa");
+	  Scanner entrada = new Scanner(System.in);
+	  
+	  while(true)
+	  {
+		  System.out.println("Informe oque vc deseja fazer: \n\tS - Para retornar valor de saldo \n\tG - Para adicionar novo gasto \n\tR - para adicionar um novo rendimento \n\tQ - Para encerrar o programa");
+		  
+		  
+		  String simbolo = entrada.next();
+		  if (simbolo.equals("S")|| simbolo.equals("s"))
+		  {
+			  // rotina para buscar saldo disponivel
+			  System.out.println("rotina para buscar saldo disponivel");
+		  }
+		  
+		  else if (simbolo.equals("G")|| simbolo.equals("g"))
+		  {
+			  // rotina para um novo gasto
+		  }
+		  
+		  else if (simbolo.equals("R")|| simbolo.equals("r"))
+		  {
+			  // rotina para uma nova receita
+		  }
+		  
+		  else if (simbolo.equals("Q")|| simbolo.equals("q"))
+		  {
+			  break;
+		  }
+	  }
+	  
+	  
+	  
+  
+
+	  
+	  
+  }
 }
 
-
+  
---
+++ src/br/edu/senai/modelo/EntradaESaida.java
@@ -0,0 +1,43 @@
+package br.edu.senai.modelo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EntradaESaida {
+
+	public void run() {
+
+	    String arquivoCSV = "file.SCV";
+	    BufferedReader br = null;
+	    String linha = "";
+	    String csvDivisor = ",";
+	    try {
+
+	        br = new BufferedReader(new FileReader(arquivoCSV));
+	        while ((linha = br.readLine()) != null) {
+
+	            String[] pais = linha.split(csvDivisor);
+
+	            System.out.println("País [code= " + pais[pais.length-4] 
+	                                 + " , name=" + pais[pais.length-1] + "]");
+
+	        }
+
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (br != null) {
+	            try {
+	                br.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	  }
+
+}
