d8e4036d9f0c96d6b8aea98b7e02e5947c12e4e1
Jean <lopesjean.jl@gmail.com>
2019-12-03 21:21:15 -0300

    Adicionado arquivo .SCV

--- src/br/edu/senai/controle/Main.java
+++ src/br/edu/senai/controle/Main.java
@@ -7,7 +7,7 @@ import br.edu.senai.modelo.ManipuladorArquivo;
 public class Main {
  
     public static void main(String args[]) throws IOException {
-        String path = "C:/Users/Senai/Documents/Jean/ControleFinanceiro/file.txt";
+        String path = "C:/Users/Senai/Documents/Jean/ControleFinanceiro/file.SCV";
  
         ManipuladorArquivo.leitor(path);
         ManipuladorArquivo.escritor(path);
